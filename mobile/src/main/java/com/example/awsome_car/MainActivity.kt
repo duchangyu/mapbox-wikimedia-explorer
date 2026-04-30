699221475ee3a2e4c8f17a90395858fa241ead88
duchangyu <duchangyu@bimsop.com>
2026-04-30 16:39:39 +0800

    feat: integrate Mapbox Maps SDK and add MapView to MainActivity

---
+++ mobile/src/main/java/com/example/awsome_car/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.awsome_car
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mapbox.maps.MapView
+import com.mapbox.maps.Style
+
+class MainActivity : AppCompatActivity() {
+    private var mapView: MapView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        
+        mapView = findViewById(R.id.mapView)
+        mapView?.mapboxMap?.loadStyle(Style.MAPBOX_STREETS)
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
